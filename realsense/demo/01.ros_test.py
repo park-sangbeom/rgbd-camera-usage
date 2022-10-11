85f6d0710153bea2db7ba16a6546b3edc4ce17aa
sangbeom <sangbeom-park@korea.ac.kr>
2022-10-12 00:14:17 +0900

    update data

--- realsense/demo/01.ros_test.py
+++ realsense/demo/01.ros_test.py
@@ -13,5 +13,5 @@ if __name__ =="__main__":
     rospy.init_node('Realsense')
     mode     = "depth"
     camera   = RealsenseD435i(mode=mode)
-    name = "test2"
+    name = "test_80_cylinder14"
     save_depth_img(msg_depth = camera.depth_image, name = name) 
